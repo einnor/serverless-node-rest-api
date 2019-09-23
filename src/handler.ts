1309d225f0a853df7f38db7e99ede89506218e04
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-09-23 18:04:42 +0300

    Export all the functions

---
+++ src/handler.ts
@@ -0,0 +1,5 @@
+export { create } from './functions/create';
+export { get } from './functions/get';
+export { list } from './functions/list';
+export { update } from './functions/update';
+export { remove } from './functions/remove';
